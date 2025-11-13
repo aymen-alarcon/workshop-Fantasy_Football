e48368ee073fac8de503b75a418e865c43c605e7
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-11-13 15:10:13 +0100

    `Added data to data.json and implemented dynamic player loading in script.js`

--- script.js
+++ script.js
@@ -23,3 +23,26 @@ if(editButton){
         document.querySelector(".modal").classList.remove("centered-modal")
     })
 }
+
+function displayPlayers(player) {
+    return `
+        <div class="${player.position}">
+            <div class="player-card d-flex justify-content-between">
+                <div class="name w-50">${player.name}</div>
+                <div class="price">${player.price}</div>
+                <div class="price">${player.team}</div>
+                <i class="bi bi-plus"></i>
+            </div>
+        </div>
+    `
+}
+
+async function loadPlayers(file) {
+    let response = await fetch(file);
+    let players = await response.json();
+    players.forEach(player => {
+       document.querySelector(".players").innerHTML += displayPlayers(player)
+    });
+}
+
+loadPlayers("data.json")
