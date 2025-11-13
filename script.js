72fec3b6c18a5a764100cd7f61b995f04ed00beb
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-11-13 17:22:17 +0100

    `Updated player data and UI to match new data.json file`

--- script.js
+++ script.js
@@ -24,24 +24,55 @@ if(editButton){
     })
 }
 
+let playersAdded = []
+
 function displayPlayers(player) {
     return `
         <div class="${player.position}">
-            <div class="player-card d-flex justify-content-between">
-                <div class="name w-50">${player.name}</div>
-                <div class="price">${player.price}</div>
-                <div class="price">${player.team}</div>
+            <div id="${player.id}" class="player-card d-flex justify-content-between">
+                <div class="name${player.id} w-50">${player.name}</div>
+                <div class="price${player.id}">${player.price}</div>
+                <div class="team${player.id}">${player.team}</div>
                 <i class="bi bi-plus"></i>
             </div>
         </div>
     `
 }
 
+function addPlayer(player) {
+    if (!document.querySelectorAll('.bi-plus')) return
+    document.querySelectorAll('.bi-plus').forEach(btn =>{
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+            let chosenPlayer = {
+                nom: document.querySelector(`.name${player.id}`).textContent,
+                price: document.querySelector(`.price${player.id}`).textContent,
+                team: document.querySelector(`.team${player.id}`).textContent,
+            };
+            console.log(chosenPlayer);
+            playersAdded.push(chosenPlayer);
+            localStorage.setItem('player', JSON.stringify(playersAdded));
+        });
+    })  
+}
+
+                                // <div class="card-body text-center">
+                                //     <img src="assets/img/vinijr.webp" class="w-25 h-100 rounded-circle" alt="">
+                                // </div>
+                                // <div class="card-text text-center">
+                                //     <i class="bi bi-x float-end"></i>
+                                //     <div class="player-name">Thibaut Courtois</div>
+                                //     <div class="team">Real Madrid</div>
+                                //     <div class="price">$6m</div>
+                                // </div>
+
+
 async function loadPlayers(file) {
     let response = await fetch(file);
     let players = await response.json();
     players.forEach(player => {
-       document.querySelector(".players").innerHTML += displayPlayers(player)
+       document.querySelector(".players").innerHTML += displayPlayers(player);
+       addPlayer(player)
     });
 }
 
